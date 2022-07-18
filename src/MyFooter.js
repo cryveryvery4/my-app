403b5f3e6bee83c4de194a8629ddb6e87782c06a
cryveryvery4 <cryveryvery4@gmail.com>
2022-07-18 10:49:50 +0700

    add header and footer

---
+++ src/MyFooter.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function MyFooter() {
+    return (
+      <div className='My-footer'>
+        <span>developed by natthawat CH.@ 2022</span>
+      </div>
+    );
+  }
+
+  export default MyFooter;
---
+++ src/Myheader.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function MyHeader(){
+    return (
+      <div className='My-header'>
+        <h1>react Demo site</h1>
+      </div>
+    );
+  }
+
+  export default MyHeader;
