50ea5263e76a505f47558ae22aec4dd612d8a52d
cryveryvery4 <cryveryvery4@gmail.com>
2022-07-25 11:43:47 +0700

    add waadwq

--- src/App.js
+++ src/App.js
@@ -2,17 +2,58 @@
 import './App.css';
 import MyHeader from './Myheader';
 import MyFooter from './MyFooter';
+import React, { useState } from 'react';
+
 function App() {
+  const [num, setNum] = useState(0);
+  const [primeR, setPrime] = useState("");
+ 
+  function AddPrimeNumber() {
+    let prime = true;
+    let num_x = num + 1;
+    setNum(num_x);
+    if (num_x == 0 || num_x == 1) setPrime("ไม่เป็นจำนวนเฉพาะ")
+    else {
+      for (let i = 2; i < num_x; i++) {
+        if (num_x % i == 0) {
+          prime = false;
+          break;
+        }
+      }
+      if (prime) setPrime("เป็นจำนวนเฉพาะ");
+      else setPrime("ไม่เป็นจำนวนเฉพาะ");
+    }
+  }
+
+  function SubtractPrimeNumber() {
+    let prime = true;
+    let num_x = num - 1;
+    setNum(num_x);
+    if (num_x == 0 || num_x == 1) setPrime("ไม่เป็นจำนวนเฉพาะ")
+    else {
+      for (let i = 2; i < num_x; i++) {
+        if (num_x % i == 0) {
+          prime = false;
+          break;
+        }
+      }
+      if (prime) setPrime("เป็นจำนวนเฉพาะ");
+      else setPrime("ไม่เป็นจำนวนเฉพาะ");
+    }
+  }
   return (
-    <div className='App'>
+    <div>
       <MyHeader />
       <div className='My-content'>
-      <h1>test</h1>
-      </div>
+        <h2>{num}</h2>
+        <button className='btn btn-primary' onClick={AddPrimeNumber}>เพิ่ม</button>&nbsp;
+        <button className='btn btn-danger' onClick={SubtractPrimeNumber}>ลด</button>
+        <div>&nbsp;</div>
+        <p>{num} {primeR}</p>
+      </div>     
       <MyFooter />
     </div>
   );
 }
 
 export default App;
-
